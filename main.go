33cc42fdfeba3ce1c4ad656ef0a705997a8f8fee
Gustavo Daniel <gugadanielalvez@gmail.com>
2024-12-05 22:04:05 -0300

    implemented a simple ebiten program

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+// Window constants
+const (
+	screenWidth  = 1280
+	screenHeight = 720
+	windowTitle  = "Snakers"
+	isResizeable = false
+)
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Hello, World!")
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle(windowTitle)
+
+	g := &Game{}
+	err := ebiten.RunGame(g)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
